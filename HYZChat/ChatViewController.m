47aac51fbfd97f45c04212db0ed3d0c78010084f
黄亚州 <1024105345@qq.com>
2017-12-15 17:32:37 +0800

    优化动画

--- HYZChat/ChatViewController.m
+++ HYZChat/ChatViewController.m
@@ -17,16 +17,24 @@
 #import "ChatManager.h"
 
 @interface ChatViewController ()
+/** 列表的数据源 */
 @property (strong, nonatomic) IBOutlet ChatDataSource *chatDataSource;
-
+/** 聊天表格的背景图 */
 @property (weak, nonatomic) IBOutlet UIImageView *imgBg;
+/** 聊天表格 */
 @property (weak, nonatomic) IBOutlet UITableView *chatTableView;
+/** 底部功能视图 */
 @property (weak, nonatomic) IBOutlet UIView *viewBottom;
+/** 底部功能视图的高度 */
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewBottomConstraintHeight;
 
+/** 消息的属性 */
 @property (strong, nonatomic) ChatMessageAttribute *messageAttribute;
+/** 输入框的高度 */
 @property (assign, nonatomic) CGFloat inputTextViewHeight;
+/** 键盘的高度 */
 @property (assign, nonatomic) CGFloat kbHeight;
+/** 表情视图的高度 */
 @property (assign, nonatomic) CGFloat emotionViewHeiht;
 
 @end
@@ -45,9 +53,18 @@
     [self registerMessageNotification];
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     [self removeMessageNoitication];
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    [self scrollTableViewToBottom];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -75,6 +92,7 @@
 
 #pragma mark - message notification
 
+/** 注册消息通知 */
 - (void)registerMessageNotification {
     //注册键盘出现通知
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector (keyboardWillShow:)
@@ -91,6 +109,7 @@
                                                  name:NotiUpdateChatViewForSendMsg object:nil];
 }
 
+/** 移除消息通知 */
 - (void)removeMessageNoitication {
     //解除键盘出现通知
     [[NSNotificationCenter defaultCenter] removeObserver:self name: UIKeyboardWillShowNotification object:nil];
@@ -102,7 +121,7 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self name:NotiUpdateChatViewForSendMsg object:nil];
 }
 
-//键盘弹出
+/** 键盘弹出 */
 - (void)keyboardWillShow:(NSNotification *)notification {
     [ChatManager sharedManager].bottomMode = ChatBottomTargetText;
     [[NSNotificationCenter defaultCenter] postNotificationName:NotiEmotionBtnDefaultStauts object:nil];
@@ -111,11 +130,14 @@
     kbRect = [self.view convertRect:kbRect fromView:nil];
     [UIView animateWithDuration:chatAnimateDuration delay:0.0 options:UIViewAnimationOptionCurveEaseOut animations:^{
         self.viewBottomConstraintHeight.constant = self.inputTextViewHeight + kbRect.size.height;
-    } completion:^(BOOL finished) {}];
+    } completion:^(BOOL finished) {
+        [self scrollTableViewToBottom];
+    }];
+    
     self.kbHeight = kbRect.size.height;
 }
 
-//键盘隐藏
+/** 键盘隐藏 */
 - (void)keyboardWillHide:(NSNotification *)notification {
     [UIView animateWithDuration:chatAnimateDuration animations:^{
         self.viewBottomConstraintHeight.constant = self.inputTextViewHeight;
@@ -123,7 +145,7 @@
     self.kbHeight = 0.0;
 }
 
-//viewbottom 高度发生变化
+/** viewbottom 高度发生变化 */
 - (void)handleNotiInputViewFrameChanage:(NSNotification *)notification {
     if (notification.object != nil && [notification.object isKindOfClass:[InputViewFrameChanageData class]]) {
         InputViewFrameChanageData *data = notification.object;
@@ -133,8 +155,11 @@
                     self.viewBottomConstraintHeight.constant = self.emotionViewHeiht + data.inputTextViewHeight;
                 else if ([ChatManager sharedManager].bottomMode == ChatBottomTargetText)
                     self.viewBottomConstraintHeight.constant = self.kbHeight + data.inputTextViewHeight;
+                else
+                    self.viewBottomConstraintHeight.constant = data.inputViewHeight;
+                [self.view layoutIfNeeded];
             } completion:^(BOOL finished) {
-//                [[NSNotificationCenter defaultCenter] postNotificationName:NotiLiveshowInteractionScrollCellToBottom object:nil];
+                [self scrollTableViewToBottom];
             }];
         }
         else {
@@ -143,12 +168,14 @@
             } completion:^(BOOL finished) {
                 if ([ChatManager sharedManager].bottomMode == ChatBottomTargetEmotion)
                     self.emotionViewHeiht = data.inputViewHeight - data.inputTextViewHeight;
+                [self scrollTableViewToBottom];
             }];
         }
         self.inputTextViewHeight = data.inputTextViewHeight;
     }
 }
 
+/** 底部功能区 */
 - (void)handleNotiChatBottomFunctionButtonClick:(NSNotification *)notification {
     if (notification.object != nil) {
         NSInteger btnValue = [notification.object integerValue];
@@ -186,6 +213,7 @@
     }
 }
 
+/** 通过发送消息更新聊天表格 */
 - (void)handleNotiUpdateChatViewForSendMsg:(NSNotification *)notification {
     if (notification.userInfo != nil) {
         ChatMsgType msgType = [notification.userInfo[@"type"] integerValue];
@@ -194,6 +222,15 @@
         [self.chatDataSource addChatMsg:msgType withMsgContent:content];
         [self.chatTableView reloadData];
     }
+}
+
+#pragma mark - 私有方法
+
+/** 将列表信息滑动到底部 */
+- (void)scrollTableViewToBottom {
+    if ([self.chatDataSource isEmptyData] == NO)
+        [self.chatTableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:self.chatDataSource.chatMsgArr.count - 1 inSection:0]
+                                  atScrollPosition:UITableViewScrollPositionBottom animated:YES];
 }
 
 #pragma mark - chat bottom function button jump
