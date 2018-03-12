4e78e33dc6ca7337f7a1d9988fe1a21ee48b37ce
黄亚州 <1024105345@qq.com>
2018-03-12 16:52:00 +0800

    添加朋友圈的跳转

---
+++ HYZChat/DiscoverController.h
@@ -0,0 +1,13 @@
+//
+//  DiscoverController.h
+//  HYZChat
+//
+//  Created by 黄亚州 on 2018/3/12.
+//  Copyright © 2018年 黄亚州. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscoverController : UITableViewController
+
+@end
---
+++ HYZChat/DiscoverController.m
@@ -0,0 +1,52 @@
+//
+//  DiscoverController.m
+//  HYZChat
+//
+//  Created by 黄亚州 on 2018/3/12.
+//  Copyright © 2018年 黄亚州. All rights reserved.
+//
+
+#import "DiscoverController.h"
+
+@interface DiscoverController ()
+
+@end
+
+@implementation DiscoverController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.tableView.tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.view.frame.size.width, 15.0f)];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view delegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44.0f;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+@end
